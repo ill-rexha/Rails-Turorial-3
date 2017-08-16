5c96e5ab172376db751d9aa2a92bd54142034431
yoshiki0000 <xxx.japan.0101@gmail.com>
2017-08-16 20:17:24 +0900

    add setup method

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,12 +1,17 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+	def setup
+		@base_title = "Ruby on Rails Tutorial Sample App"
+	end
+
   test "should get home" do
     get static_pages_home_url
     # Http status check
     assert_response :success
     # operate as selecter
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
@@ -14,7 +19,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     # Http status check
     assert_response :success
     # operate as selecter
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
@@ -22,7 +27,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   	# Http status check
   	assert_response :success
   	# operate as selecter
-  	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  	assert_select "title", "About | #{@base_title}"
   end
 
 end
