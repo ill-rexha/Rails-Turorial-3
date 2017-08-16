6e9a59eae57c46142edcc9a31eaabe9b26de462b
yoshiki0000 <xxx.japan.0101@gmail.com>
2017-08-16 20:03:47 +0900

    Code to make some tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,12 +3,26 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
+    # Http status check
     assert_response :success
+    # operate as selecter
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get static_pages_help_url
+    # Http status check
     assert_response :success
+    # operate as selecter
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get about" do
+  	get static_pages_about_url
+  	# Http status check
+  	assert_response :success
+  	# operate as selecter
+  	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
 end
