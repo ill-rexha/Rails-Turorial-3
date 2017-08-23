1d7c340e0dddbdd2c12e32a046080e6771480385
yoshiki0000 <xxx.japan.0101@gmail.com>
2017-08-24 02:16:27 +0900

    finish user signup flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,9 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
 
   	if @user.save
-
+  		flash[:success] = "Welcome to the Sample App!"
+  		#ユーザーページにリダイレクトさせる。
+  		redirect_to @user
   	else
   		render 'new'
   	end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
 
+  post '/signup',  to: 'users#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
