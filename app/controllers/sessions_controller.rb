a46a0ad1bfabcdc44c10ceda61f136253a800a78
yoshiki0000 <xxx.japan.0101@gmail.com>
2017-08-24 22:42:09 +0900

    add more test into an integration login test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,11 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
   	# only success if system find usr by email and password authenticate
-  	if user && user.authenticate(paramns[:session][:password])
-
-  	else 
-    flash[:danger] = 'Invalid email/password combination'
+  	if user && user.authenticate(params[:session][:password])
+  	log_in user
+  	redirect_to user
+  	else
+    flash.now[:danger] = 'Invalid email/password combination'
   	render "new"
   	end
   end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,14 @@
 module SessionsHelper
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+	# ログインしているUserがいる場合
+	def current_user
+		current_user ||= User.find_by(id: session[:user_id])
+	end
+
+	# ログインしている場合としていない場合
+	def logged_in?
+		!current_user.nil?
+	end
 end
