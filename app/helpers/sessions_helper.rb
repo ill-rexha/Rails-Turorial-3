aed89f9f4698ecbdb2483b0910473e412a1703b1
yoshiki0000 <xxx.japan.0101@gmail.com>
2017-08-25 19:27:12 +0900

    add method each cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,31 @@
 module SessionsHelper
+	
+	# 渡されたUserでログインする。
 	def log_in(user)
 		session[:user_id] = user.id
 	end
-	# ログインしているUserがいる場合
+	
+	# ユーザーのセッションを永久化する。
+	def remember(user)
+		user.remember
+		cookie.permanent.signed[:user_id] = user.id
+		cookie.permanent[remember_token] = user.remember_token
+	end
+
+	# 記憶トークンcookieに対応するユーザーを返す
 	def current_user
-		current_user ||= User.find_by(id: session[:user_id])
+		# ログインしているUserがいる場合
+		if(user_id = session[:user_id])
+			@current_user ||= User.find_by(id: user_id)
+			
+		elsif (user_id = cookies.signed[:user_id])
+			  user = User.find_by(id: user_id)
+			if user && user.authenticated?(cookies[:remember_token])
+				log_in user
+				@current_user = user
+			end
+		end
+
 	end
 
 	# ログインしている場合としていない場合
@@ -16,4 +37,5 @@ module SessionsHelper
 		session.delete(:user_id)
 		@current_user = nil
 	end
+
 end
