db7054de93369fed69ed66be1a900789b3143b19
yoshiki0000 <xxx.japan.0101@gmail.com>
2017-08-25 19:13:45 +0900

    add random token , remember and authenticate method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	attr_accessor :remember_token
 	# 名前が存在していることを確認し、最大文字数を50文字までに制限
 	validates(:name, presence: true,length:{maximum:50})
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -11,14 +12,26 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }
 
-	def User.digest(string)
+	# 渡された文字列のハッシュ値を返す
+	def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
  	end
-
- 	def User.new_token
+ 	
+ 	# ランダムなトークンを返す。
+ 	def self.new_token
  		SecureRandom.urlsafe_base64
+ 	end
+
+ 	# 永続セッションのためにユーザーをデータベースに記憶する
+ 	def remember
+ 		self.remember_token = User.new_token
+ 		update_attribute(:remember_digest,User.digest(remember_token))
+ 	end
+ 	# 渡されたtokenがdigestと一緒の場合は、trueを返す。
+ 	def authenticated(remember_token)
+ 		BCrypt::Password.new(remember_digest).is_password?(remember_token)
  	end
 
 end
