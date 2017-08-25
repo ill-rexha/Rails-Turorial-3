379bed15c80fd52ce95518407e0082e9c6931c17
yoshiki0000 <xxx.japan.0101@gmail.com>
2017-08-25 18:51:35 +0900

    add remember digest to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
-
+	# 名前が存在していることを確認し、最大文字数を50文字までに制限
 	validates(:name, presence: true,length:{maximum:50})
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	# dbに保存する前に、emailを小文字に
 	before_save { self.email = email.downcase}
+	# emailが存在していることを確認し、最大文字数を255文字までに制限
 	validates :email, presence: true,length:{maximum:255},
 		format: { with: VALID_EMAIL_REGEX },
 		uniqueness: { case_sensitive: false }
@@ -13,6 +15,10 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
-  end
+ 	end
+
+ 	def User.new_token
+ 		SecureRandom.urlsafe_base64
+ 	end
 
 end
