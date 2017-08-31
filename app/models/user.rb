3d71affd4af2a55b3419004e4e7d73a3ebe4f20d
yoshiki0000 <xxx.japan.0101@gmail.com>
2017-08-31 18:02:14 +0900

    add new action in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,10 @@ class User < ApplicationRecord
 	validates :password, presence: true, length: { minimum: 6 },allow_nil:true
 	# ユーザーがマイクロソフトを複数所有する。
 	has_many :microposts, dependent: :destroy
+	has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+    has_many :following, through: :active_relationships, source: :followed
 
 	# 渡された文字列のハッシュ値を返す
 	def self.digest(string)
@@ -75,6 +79,22 @@ class User < ApplicationRecord
 
 	def feed
 		Micropost.where("user_id = ?", id)
+	end
+
+	 #  code for user-following
+    #========================================================================================================================================
+	def follow(other_user)
+    	active_relationships.create(followed_id: other_user.id)
+	end
+
+	# ユーザーをフォロー解除する
+	def unfollow(other_user)
+		active_relationships.find_by(followed_id: other_user.id).destroy
+	end
+
+	# 現在のユーザーがフォローしてたらtrueを返す
+	def following?(other_user)
+	    following.include?(other_user)
 	end
 
 	private
