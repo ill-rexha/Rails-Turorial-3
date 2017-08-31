60c2c57979edb81366673ca7e58115c8d64248af
yoshiki0000 <xxx.japan.0101@gmail.com>
2017-08-31 21:58:11 +0900

    Add user following and finish rails tutorial

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,13 @@ class User < ApplicationRecord
 	has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+
+    has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+
     has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
 
 	# 渡された文字列のハッシュ値を返す
 	def self.digest(string)
@@ -78,8 +84,11 @@ class User < ApplicationRecord
 	end
 
 	def feed
-		Micropost.where("user_id = ?", id)
-	end
+	    following_ids = "SELECT followed_id FROM relationships
+	                     WHERE follower_id = :user_id"
+	    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  	end
 
 	 #  code for user-following
     #========================================================================================================================================
